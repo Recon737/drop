ed6e9d7c3f0828c3a547f59dcae385c2f1e6c142
DecDuck <declanahofmeyr@gmail.com>
2024-12-27 20:39:41 +1100

    fix(cleanup): remove app.vue and clear nuxt.config.ts of options

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,6 +1,2 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
-  ssr: false,
-});
+export default defineNuxtConfig({});
