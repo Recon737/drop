320d32388030ce7f82e0a6a36c7db54556800c80
quexeky <git@quexeky.dev>
2026-01-19 13:45:00 +1100

    feat: Add nix and rust-toolchain.toml

---
+++ cli/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Drop-OSS app development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        libraries = with pkgs; [
+            glib
+            glibc
+            openssl
+          ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            git
+            rust-bin.nightly.latest.default
+            rust-analyzer
+          ];
+
+          
+          buildInputs = libraries;
+
+          shellHook = ''
+            echo "Drop-OSS app development environment loaded."
+            export LD_LIBRARY_PATH="${
+              pkgs.lib.makeLibraryPath libraries
+            }:$LD_LIBRARY_PATH"
+          '';
+        };
+      }
+    );
+}
