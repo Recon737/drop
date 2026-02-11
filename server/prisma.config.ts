d1786b3c60eb7f6dafdd1e6badc113e42e15ba2f
Husky <39809509+Huskydog9988@users.noreply.github.com>
2026-02-10 20:26:53 -0500

    migrate to prisma v7 (#345)
    * migrate to prisma v7
    
    * fix prisma type imports
    
    * update prisma version in docker
    
    * fix prisma cli breaking things

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,10 +1,17 @@
 import { config } from "dotenv";
-import type { PrismaConfig } from "prisma";
+import { defineConfig } from "prisma/config";
 import path from "node:path";
 
+// load .env variables
 config();
 
-export default {
+// need to use built in env access as prisma's env function throws when DATABASE_URL is undefined
+// this is acceptable as prisma throws already it actually needs the URL
+const databaseURL = process.env.DATABASE_URL;
+
+export default defineConfig({
   schema: path.join("prisma"),
-  earlyAccess: true,
-} satisfies PrismaConfig;
+  datasource: {
+    url: databaseURL,
+  },
+});
